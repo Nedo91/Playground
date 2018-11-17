e4198716ec75412863db948ed3e26b51622ca907
Nedeljko Mijajlovic <stoopro2@gmail.com>
2018-11-17 17:25:35 +0100

    bools learned

---
+++ Booleans/MyPlayground.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+var amITheBestTeacherEver: Bool = true
+
+amITheBestTeacherEver = false
+
+if true == false || true == true {
+    print ("WTFFFK")
+    
+    
+}
+
+var hasDataFinishedDwnld = false
+// waitin to be done = true
+
+hasDataFinishedDwnld = true
+// load something after its done
+
+if 2 == 2 {
+    print("Should not see this")
+    
+}
+
+
+// Equal to: ==
+// Not Equal to: !=
+//   > <   <=  >=
+
+var bankBallance = 400
+var itemToBuy = 400
+
+if bankBallance >= itemToBuy {
+    print ("item Purchased")
+    
+}
+if itemToBuy > bankBallance {
+    print ("You cannot afford it")
+    
+}
+if itemToBuy == bankBallance {
+    print("Hey ziomek, masz na koncie 0 ziko")
+    
+}
+var bookTitle1 = "Harry chuj i dupa"
+var bookTitle2 = "CHARY huj i dopa"
+
+if bookTitle1 != bookTitle2 {
+    print ("naucz sie pisac ziomek")
+    
+}
